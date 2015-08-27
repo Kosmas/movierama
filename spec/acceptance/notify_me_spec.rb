580ac4b9d7d93a365a2a09e679398bc2819f1392
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-08-27 15:44:06 +0200

    add specs for movie hate

--- spec/acceptance/notify_me_spec.rb
+++ spec/acceptance/notify_me_spec.rb
@@ -3,13 +3,24 @@ require 'rails_helper'
 RSpec.describe 'notify me', type: :feature do
 
   let(:author) { User.create(uid: 'null|1234', name: 'Bob', email: 'bob@test.com') }
-  subject { NotifyMeMailer.movie_liked(author.email) }
 
   context 'when a movie is liked' do
+    subject { NotifyMeMailer.movie_liked(author.email) }
+
     it 'should be delivered to the author email when there is a like' do
       is_expected.to deliver_to(author.email)
     end
 
     it { is_expected.to have_subject(/Somebody has liked your movie/) }
+  end
+
+  context 'when a movie is hated' do
+    subject { NotifyMeMailer.movie_hated(author.email) }
+
+    it 'should be delivered to the author email when there is a hate' do
+      is_expected.to deliver_to(author.email)
+    end
+
+    it { is_expected.to have_subject(/Somebody has hated your movie/) }
   end
 end
